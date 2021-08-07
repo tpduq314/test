b384a0ee74a7f35a288a30988b6e9da8e973aed5
USER <USER@DESKTOP-PDI03CH>
2021-08-07 10:40:46 +0900

    test

---
+++ WebContent/js/my.js
@@ -0,0 +1,11 @@
+/**
+ *my.js 
+ */
+
+var name="홍길동";
+var subject="자바스크립트";
+address="서울시 동작구";
+
+document.write("이름 : "+name+"<hr/>");  //한 문장의 끝을 의미하고 생략 가능함. +는 문자열 연결 연산기호
+document.write("추천과목 : "+subject+"<hr/>");
+document.write("주소 : "+address+"<hr/>");
